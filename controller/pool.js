4308bd221ababd0514172199e8a68da67cb11391
RealToma <real.toma.contact@gmail.com>
2024-07-29 09:27:51 -0400

    implmeented getting txlists using both of scan and alchemy's apiendpoints

--- controller/pool.js
+++ controller/pool.js
@@ -31,8 +31,59 @@ router.post("/list", async (req, res) => {
         };
         let resPoolDetail = await axios(configPoolDetail);
         let dataPoolDetail = resPoolDetail.data.data;
-        if (resPoolDetail.status === 200 && dataPoolDetail.length !== 0)
-          console.log("dataPoolDetail:", dataPoolDetail);
+        if (resPoolDetail.status === 200 && dataPoolDetail.length !== 0) {
+          console.log("dataPoolDetail:", dataPoolDetail.network);
+          const networkPoolDetail = dataPoolDetail.network;
+          switch (networkPoolDetail) {
+            case "Ethereum":
+              // using scan's api endpoitns:
+              // const configScanPoolTxLists = {
+              //   method: "get",
+              //   url: process.env.API_URL_ETHERSCAN + `/api`,
+              //   params: {
+              //     module: "account",
+              //     action: "txlist",
+              //     address: dataPoolDetail.contract,
+              //     startblock: 0,
+              //     endblock: 99999999,
+              //     sort: "asc",
+              //     apikey: "GKT561CCZZB4PWB94HAPCSAD1R5BPXQTUA",
+              //   },
+              // };
+              // const resTxLists = await axios(configScanPoolTxLists);
+              // console.log("resTxLists:", resTxLists.data.result);
+
+              // using alchemy's api endpoint:
+              const configAlchemyPoolLists = {
+                apiKey: process.env.API_KEY_ALCHEMY,
+                network: Network.ETH_MAINNET,
+              };
+
+              const alchemyPoolLists = new Alchemy(configAlchemyPoolLists);
+              const addressContract = dataPoolDetail.contract;
+              const resAlchemyPoolLists = await alchemyPoolLists.core.getAssetTransfers({
+                fromBlock: "0x0",
+                // fromAddress: "0x0000000000000000000000000000000000000000",
+                to: addressContract,
+                excludeZeroValue: true,
+                category: [
+                  "external",
+                  "internal",
+                  "erc20",
+                  "erc721",
+                  "erc1155",
+                ], // "external", "internal", "erc20", "erc721", "erc1155"
+              });
+
+              console.log("resAlchemyPoolLists:", resAlchemyPoolLists.transfers);
+
+            // case "Base":
+
+            // case "Polygon":
+            default:
+              break;
+          }
+        }
         // arrayPoolsList.push(each);
       });
     }
